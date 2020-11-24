9a530af9fa87829ac571e3a34a056357243b890d
Viverson Cunha <zevives@gmail.com>
2020-11-24 00:34:09 -0300

    definindo direções

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0 , 16 * box , 16 * box);
+    context.fillRect(0 , 0 , 16 * box , 16 * box);
 }
 
 function criarCobrinha(){
@@ -21,7 +21,21 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown',update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+} 
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;    
+
     criarBG();
     criarCobrinha();
 
@@ -44,3 +58,5 @@ function iniciarJogo(){
 
 
 let jogo = setInterval(iniciarJogo,100);
+
+
